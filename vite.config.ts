6622f655b817af4567f8e83a65019cabbe74f3b7
kosdfsd <kathywchapman442@gmail.com>
2025-03-09 17:13:51 +0000

    add to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build', // Change this to 'build' from the default 'dist'
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
