c88b7fba94ed8e9d5211410e91b07669ebc484ee
ama-afk <marlinwang4@gmail.com>
2025-04-18 23:21:16 +0100

    Add files via upload

---
+++ assets/textarea-C67IKOP1.js
@@ -0,0 +1,6 @@
+import{c as o,e as n,f as d,j as c}from"./index-CR3Du1ed.js";/**
+ * @license lucide-react v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]],x=o("Copy",l);function p(s){const e=n.c(8);let t,i;e[0]!==s?({className:t,...i}=s,e[0]=s,e[1]=t,e[2]=i):(t=e[1],i=e[2]);let r;e[3]!==t?(r=d("border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",t),e[3]=t,e[4]=r):r=e[4];let a;return e[5]!==i||e[6]!==r?(a=c.jsx("textarea",{"data-slot":"textarea",className:r,...i}),e[5]=i,e[6]=r,e[7]=a):a=e[7],a}export{x as C,p as T};
